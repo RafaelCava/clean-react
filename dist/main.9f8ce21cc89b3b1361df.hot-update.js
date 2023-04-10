cca27d090f249c0e71dd6569d5e8aa0e87185a74
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-09 21:23:04 -0300

    feat: add login view

---
+++ dist/main.9f8ce21cc89b3b1361df.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdateclean_react"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!./node_modules/sass-loader/dist/cjs.js!./src/presentation/pages/login/login-styles.scss":
+/*!*****************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!./node_modules/sass-loader/dist/cjs.js!./src/presentation/pages/login/login-styles.scss ***!
+  \*****************************************************************************************************************************************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable: \"$background\".\n        on line 10 of src/presentation/pages/login/login-styles.scss\n>>   background-color: $background;\n\n   --------------------^\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("5cfb743abdd3ec71c2e2")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi45ZjhjZTIxY2M4OWIzYjEzNjFkZi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2NsZWFuLXJlYWN0L3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJzb3VyY2VzQ29udGVudCI6WyJfX3dlYnBhY2tfcmVxdWlyZV9fLmggPSAoKSA9PiAoXCI1Y2ZiNzQzYWJkZDNlYzcxYzJlMlwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
