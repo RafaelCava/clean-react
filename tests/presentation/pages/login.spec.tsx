03a8f47ca1263ed20782d5481e41f301d5eb451d
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-10 21:54:03 -0300

    refactor: move SUT creation to a factory method

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,27 +1,38 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { type RenderResult, render } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 
 describe('Login', () => {
   it('should render', () => {
-    const sut = render(<Login />)
+    const { sut } = makeSut()
     expect(sut).toBeTruthy()
   })
 
   it('should not render spinner and error on initial state', () => {
-    const sut = render(<Login />)
+    const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
 
   it('should start with initial state', () => {
-    const sut = render(<Login />)
+    const { sut } = makeSut()
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
 
   it('should render the correct title on inputs with initial state', () => {
-    const sut = render(<Login />)
+    const { sut } = makeSut()
     const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
