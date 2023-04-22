a260eb9d33b4c6f7f96f866482160d20364590a0
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-22 20:17:25 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

--- src/validation/validators/required-field-validation.ts
+++ src/validation/validators/required-field-validation.ts
@@ -1,8 +1,9 @@
+import { RequiredFieldError } from '../errors'
 import { type FieldValidation } from '../protocols'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
   validate (value: string): Error | null {
-    return value ? null : new Error('Campo obrigatório')
+    return value ? null : new RequiredFieldError()
   }
 }
--- tests/validation/validators/required-field-validation.spec.ts
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -1,17 +1,19 @@
 import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
 
-const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation('any_field')
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('Required field validation', () => {
   it('should return error if field is empty', () => {
     const sut = makeSut()
     const error = sut.validate('')
-    expect(error).toEqual(new Error('Campo obrigatório'))
+    expect(error).toEqual(new RequiredFieldError())
   })
 
   it('should return null on succeeds', () => {
     const sut = makeSut()
-    const error = sut.validate('any_value')
+    const error = sut.validate(faker.random.word())
     expect(error).toBeNull()
   })
 })
