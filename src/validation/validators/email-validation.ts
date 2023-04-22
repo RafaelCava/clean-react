7445cc301545e99c37d5154d96770bff45e29cfa
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-22 20:33:42 -0300

    feat: ensure EmailValidation return null if email is valid

--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -6,9 +6,6 @@ export class EmailValidation implements FieldValidation {
 
   validate (value: string): Error | null {
     const exp = /^[a-zA-Z0-9._%+-]+(?:\.[a-zA-Z0-9._%+-]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?<!\.{2})$/g
-    if (exp.test(value)) {
-      return null
-    }
-    return new InvalidFieldError()
+    return exp.test(value) ? null : new InvalidFieldError()
   }
 }
--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -10,4 +10,10 @@ describe('EmailValidation', () => {
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  it('should return null if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeNull()
+  })
 })
