513bee23fd815b9920dcd97f6bf6d931c517b70c
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-23 11:19:41 -0300

    feat: ensure ValidationBuilder return EmailValidation

--- tests/validation/validators/validation-builder.spec.ts
+++ tests/validation/validators/validation-builder.spec.ts
@@ -1,9 +1,15 @@
-import { RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
+import { EmailValidation, RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
 
 describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidation', () => {
     const field = 'any_field'
     const validators = sut.field(field).required().build()
     expect(validators).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('should return EmailValidation', () => {
+    const field = 'any_field'
+    const validators = sut.field(field).email().build()
+    expect(validators).toEqual([new EmailValidation(field)])
   })
 })
