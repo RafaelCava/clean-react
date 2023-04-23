458f75805b8298ad8ca33c089fedc92c3a7e8b09
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-23 11:24:11 -0300

    test: refactor tests

--- tests/validation/validators/validation-builder.spec.ts
+++ tests/validation/validators/validation-builder.spec.ts
@@ -1,21 +1,34 @@
 import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
+import { faker } from '@faker-js/faker'
 
 describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidation', () => {
-    const field = 'any_field'
+    const field = faker.database.column()
     const validators = sut.field(field).required().build()
     expect(validators).toEqual([new RequiredFieldValidation(field)])
   })
 
   it('should return EmailValidation', () => {
-    const field = 'any_field'
+    const field = faker.database.column()
     const validators = sut.field(field).email().build()
     expect(validators).toEqual([new EmailValidation(field)])
   })
 
   it('should return MinLengthValidation', () => {
-    const field = 'any_field'
-    const validators = sut.field(field).min(10).build()
-    expect(validators).toEqual([new MinLengthValidation(field, 10)])
+    const field = faker.database.column()
+    const length = Number(faker.random.numeric())
+    const validators = sut.field(field).min(length).build()
+    expect(validators).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  it('should return a list of validations', () => {
+    const field = faker.database.column()
+    const length = Number(faker.random.numeric())
+    const validators = sut.field(field).required().email().min(length).build()
+    expect(validators).toEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthValidation(field, length)
+    ])
   })
 })
